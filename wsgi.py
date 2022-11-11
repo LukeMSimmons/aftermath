fc2e8ca2965834d918dbe8a82ef2f610089aba3c
Luke Simmons <TheRealLukeSimmons@gmail.com>
2022-11-11 12:01:25 -0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import sys
 
 # add your project directory to the sys.path
-project_home = u'/home/aslasdfasdfasdfasdfasdgfadfgsdtg/aftermath_test'
+project_home = u'/home/aftermath/aftermath_test'
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
