fda566c7de3297df0932dd4288e8136c5eba5e24
Grandviewsurfer <Luke.Michael.Simmons@gmail.com>
2022-11-11 16:21:04 -0800

    cleaned wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,3 @@
 from app import app 
-import sys
-
-# Add project directory to sys.path
-project_home = u'/home/aftermath/aftermath'
-if project_home not in sys.path:
-    sys.path = [project_home] + sys.path
 
 application = app.server
