ff5e6ec47861bab4489e33f3eedbca11a36c9231
Grandviewsurfer <Luke.Michael.Simmons@gmail.com>
2022-11-11 16:14:33 -0800

    minor commit

--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,6 @@
 import dash; from dash import html, dcc, callback, Input, Output 
 
-# Register to app.py
+# Register to app.py 
 title = 'Aftermath Dashboards' 
 dash.register_page(__name__, title=title) 
 
