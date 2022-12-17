14627f2ffe2e2a57c66f8e0decd9cd3981a679c2
Grandviewsurfer <Luke.Michael.Simmons@gmail.com>
2022-12-16 17:02:27 -0800

    mvp

--- config.py
+++ config.py
@@ -6,7 +6,11 @@ buff = '14px'
 
 sig = html.Div([
         html.Div([
-            html.A('contact', href='https://linkedin.com/in/lukemsimmons', 
-                style=dict(color='darkcyan', textDecoration='none'))], 
-            style=dict(marginLeft=buff))], 
-        style=dict(color='#595959')) 
+            html.H4('For more information, or a quote, send an email to:')], 
+            style=dict(marginLeft=buff, display='inline-block')), 
+        html.Div([
+            html.A('contactlukesimmons@gmail.com', 
+                   href='https://linkedin.com/in/lukemsimmons', 
+                   style=dict(color='darkcyan', textDecoration='none'))], 
+            style=dict(marginLeft='4px', fontSize='20px', display='inline-block'))], 
+        style=dict(marginLeft='27%', color='#595959')) 
--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,11 @@
+from config import buff, template, font, sig 
 import dash; from dash import html, dcc, callback, Input, Output 
-from config import buff, sig 
+import plotly.express as px 
+import pandas as pd 
 
+# Get DataFrames 
+co_df = pd.read_pickle('data/pkl/co_df.pkl')
+steam_df = pd.read_pickle('data/pkl/steam_games.pkl')
 
 # Register to app.py 
 title = 'Aftermath Dashboards' 
@@ -10,13 +15,125 @@ dash.register_page(__name__, title=title)
 layout = html.Div([
 
     html.Div([
-        html.H1(title)], 
+        html.Div([
+            html.Img(src='assets/aftermath_logo.png')], 
+            style=dict(marginLeft='8px', display='inline-block')), 
+        html.Div([
+            html.H1(title)], 
+            style=dict(marginLeft='14px', display='inline-block'))], 
+        style=dict(height='58px')), 
+    
+    html.Div([
+        html.Div([
+            html.H3('Do you have raw data, but want insights instead?')]), 
+        html.Div([
+            html.H3('A web app like this one might just be right for you!')])], 
+        style=dict(marginLeft=buff, height='82px')), 
+
+    html.Div([
+        html.H3('You may want to analyze one numerical variable, perhaps with a histogram.')], 
         style=dict(marginLeft=buff)), 
 
     html.Div([
-        html.H2('Do you have lots of data, but want a dashboard instead?')], 
+        html.Div([
+            html.H4('Select a variable'), 
+            dcc.Dropdown(steam_df.columns, 'Metacritic Score', id='steam_dropdown')], 
+            style=dict(marginLeft=buff, width='10%', display='inline-block')), 
+        html.Div([
+            html.H4('Marginal Plot Type'), 
+            dcc.Dropdown(['box', 'violin', 'rug', 'None'], 'box', id='steam_m_dropdown')], 
+            style=dict(marginLeft=buff, width='10%', display='inline-block'))]), 
+
+    html.Div([
+        html.Div([
+            dcc.Graph(id='steam_histogram')], 
+            style=dict())]), 
+
+    html.Br(), 
+    html.Div([
+        html.H3('You could track variables over time with a line plot.'), 
+        html.H5('Protip: try double-clicking one of the sites in the legend.')], 
         style=dict(marginLeft=buff)), 
 
+    html.Div([
+        html.Div([
+            html.H4('Select a State'), 
+            dcc.Dropdown(co_df['State'].unique(), 'California', 
+                         id='co_state_dropdown')], 
+            style=dict(marginLeft=buff, width='10%', display='inline-block')), 
+        html.Div([
+            html.H4('Select Counties'), 
+            dcc.Dropdown(value=['San Diego'], 
+                         multi=True, id='co_county_dropdown')], 
+            style=dict(marginLeft=buff, width='12%', display='inline-block'))]), 
+
+    html.Div([
+        html.Div([
+            dcc.Graph(id='co_timeseries')], 
+            style=dict())]), 
+
+    html.Div([
+        html.H3('Have us build your own interactive dashboard, and watch the insights just ooze right out.')], 
+        style=dict(marginLeft=buff)), 
+
+    html.Br(), html.Br(), html.Br(), html.Br(), 
+    sig]) 
 
 
-    sig])
+
+# Elements with Callbacks 
+
+
+## Steam Games 
+@callback(
+    Output('steam_histogram', 'figure'), 
+    Input('steam_dropdown', 'value'), 
+    Input('steam_m_dropdown', 'value'))
+
+def steam_fig(col, marginal): 
+
+    marginal = False if marginal == 'None' else marginal 
+
+    fig = px.histogram(steam_df[steam_df[col]!=0], x=col, marginal=marginal) 
+
+    title = f'Distribution of {col} for Games on Steam'
+    fig.update_layout(title=title, title_x=.45, title_font_size=18, 
+                      template=template, font_family=font, height=600)
+    return fig 
+
+
+
+
+
+
+
+
+
+## CO Timeseries 
+@callback(
+    Output('co_county_dropdown', 'options'), 
+    Input('co_state_dropdown', 'value')) 
+
+def filter_dropdown(state): 
+    return co_df[co_df['State']==state]['County'].unique()
+
+
+## CO Timeseries 
+@callback(
+    Output('co_timeseries', 'figure'), 
+    Input('co_state_dropdown', 'value'), 
+    Input('co_county_dropdown', 'value'))
+
+def co_timeseries(state, counties): 
+    
+    dfi = co_df[(co_df['State']==state) & (co_df['County'].isin(counties))]
+
+    title = 'Daily Max Carbon Monoxide by County in 1980'
+    fig = px.line(dfi, x='Date', y='Max CO', color='Site') 
+    
+    fig.update_layout(title=title, title_x=.45, title_font_size=18, 
+                      template=template, font_family=font, height=600)
+    
+    fig.update_yaxes(title='Max CO (ppm)', rangemode='tozero')
+
+    return fig
